1b5f22e941a8c2f77c6d83851fa99b8d8fa4eb2b
John <kohler.j@husky.neu.edut>
2014-02-07 22:50:34 -0500

    first commit

---
+++ app/controllers/generic_pages_controller.rb
@@ -0,0 +1,5 @@
+class GenericPagesController < ApplicationController
+	def home
+
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gossiply::Application.config.secret_key_base = 'fe04164807a67649cbceecdc8a50e1328b622375b511b70ae2e4227da9c5df1ba9dc7b2aa7ec760c7ea49f2792316c6cf093de96b8b18ff7b53b5ded2bb56a80'
