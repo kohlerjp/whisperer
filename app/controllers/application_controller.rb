3c94d10540979d687d886800a9480b0cadfea224
John <kohler.j@husky.neu.edut>
2014-02-08 13:28:12 -0500

    json header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :allow_cross_domain_access
     def allow_cross_domain_access
-        response.headers["Access-Control-Allow-Origin"] = "*"
-        response.headers["Access-Control-Allow-Methods"] = "*"
+       response.headers["Access-Control-Allow-Headers"] = "Content-Type, X-Requested-With"
+        response.headers["Access-Control-Allow-Methods"] = "GET, PUT, POST, DELETE"
     end
 
   private
