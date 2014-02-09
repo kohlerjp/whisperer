21a505cabe169237241959b4428c2254f7f4d24f
John <kohler.j@husky.neu.edut>
2014-02-08 19:18:26 -0500

    Mentions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,20 +2,13 @@ class PostsController < ApplicationController
 
 	def create
     @post = current_user.posts.build(micropost_params)
-    text = @post.text
-    mention_ids = text.scan(/@[^ \t]*/)
-   
-
+    
     if @post.save
-          mention_ids.each do |user_id|
-      user_id = user_id.delete('@')
-      mention = Mention.new(uid: user_id, post_id: @post.id )
-      mention.save
-
-    end
-          redirect_to @post
+         mention = Mention.new(uid: params[:post][:mentioned],post_id: params[:post_id] )
+         mention.save
+        redirect_to @post
     else
-      redirect_to @post
+      redirect_to root_url
     end
   end
 
